c8f5c90ee9755c76fa3bebc71795cfd844f13aba
Prajwal7842 <prajwalgoel@gmail.com>
2022-04-08 13:11:38 +0530

    Add Test Cases

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -15,6 +15,9 @@ public class Calculator {
         return fact;
     }
     public double sqrt(double N) {
+        if(N < 0) {
+            throw new IllegalArgumentException("Invalid Input: Found Negative Number");
+        }
         double ans = Math.sqrt(N * 1.0);
         return ans;
     }
@@ -32,30 +35,33 @@ public class Calculator {
     public static void main(String args[]) {
         Calculator calObject = new Calculator();
         Scanner scannerObject = new Scanner(System.in);
-        while(1) {
+        while(true) {
             System.out.println("Welcome to Calculator App.\nPress 1 to calculate Square Root of an integer\nPress 2 to calculate Factorial of an integer." +
-                    "\nPress 3 to calculate Natural log of a number.\nPress 4 to calculate X to the power Y.");
-            int choice = sc.nextInt();
+                    "\nPress 3 to calculate Natural log of a number.\nPress 4 to calculate X to the power Y.\nPress 5 to exit the application.");
+            int choice = scannerObject.nextInt();
             if(choice == 1) {
-                double N = sc.nextDouble();
+                double N = scannerObject.nextDouble();
                 double ans = calObject.sqrt(N);
                 System.out.println("Square Root of " + N + " = " + ans);
             }
             else if(choice == 2) {
-                long N = sc.nextLong();
+                long N = scannerObject.nextLong();
                 long ans = calObject.factorial(N);
                 System.out.println("Factorial of " + N + " = " + ans);
             }
             else if(choice == 3) {
-                double N = sc.nextDouble();
+                double N = scannerObject.nextDouble();
                 double ans = calObject.log(N);
                 System.out.println("Natural Log of " + N + " = " + ans);
             }
             else if(choice == 4) {
-                long X = sc.nextInt();
-                long Y = sc.nextInt();
+                long X = scannerObject.nextInt();
+                long Y = scannerObject.nextInt();
                 long ans = calObject.power(X, Y);
                 System.out.println(X + " to the power " + Y + " = " + ans);
+            }
+            else if(choice == 5) {
+                break;
             }
             else {
                 throw new IllegalArgumentException("Invalid Choice. Exiting!");
