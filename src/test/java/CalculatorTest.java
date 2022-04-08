e2da57276b597a96cae7466199d3b9a8ffd9502d
Prajwal7842 <prajwalgoel@gmail.com>
2022-04-08 12:20:53 +0530

    Add Calculator Test Class

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+public class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void factorial() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sqrt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void power() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void log() {
+    }
+}
